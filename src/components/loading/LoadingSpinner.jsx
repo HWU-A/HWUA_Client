381fcb74c02143ac4ab7ed5d85cfa98ddc9afde7
breadman98 <swo98@naver.com>
2023-05-30 14:15:44 +0900

    loading spinner

--- src/components/loading/LoadingSpinner.jsx
+++ src/components/loading/LoadingSpinner.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const spinAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const SpinnerContainer = styled.div`
+  /* display: flex;
+  align-items: center;
+  justify-content: center; */
+  ${(props) => props.theme.flex.flexCenter}
+  height: 100vh;
+`;
+
+const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  animation: ${spinAnimation} 1s linear infinite;
+`;
 
 const LoadingSpinner = () => {
   return (
-    <div>
-      loading...
+    <SpinnerContainer>
+      <Spinner />
       {console.log("loading...")}
-    </div>
+    </SpinnerContainer>
   );
 };
 
